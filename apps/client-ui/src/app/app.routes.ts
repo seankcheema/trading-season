a7aa52fc41c23f59534c50b11a25c3e8227f8f0c
Soli Ateefa <sateefa2904@gmail.com>
2026-09-24 13:57:46 +0000

    feat(KAN-107/market-fullscreen): add market fullscreen route

--- apps/client-ui/src/app/app.routes.ts
+++ apps/client-ui/src/app/app.routes.ts
@@ -16,5 +16,10 @@ export const routes: Routes = [
     canActivate: [authGuard],
     loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
   },
+  {
+    path: 'market',
+    canActivate: [authGuard],
+    loadComponent: () => import('./market/market-fullscreen.component').then((m) => m.MarketComponent),
+  },
   { path: '**', redirectTo: '' },
 ];
